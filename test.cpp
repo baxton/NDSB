335934dc011ff6fd1ee8d10f58aaadd158f91665
Maxim Alekseykin <max.uk2005@gmail.com>
2015-06-16 16:37:21 +0800

    test for miss/dup in array problem

--- test.cpp
+++ test.cpp
@@ -1,98 +1,150 @@
-
-
-#include <cmath>
 #include <iostream>
-#include <vector>
+#include <cstdlib>
+#include <time.h>
+#include <limits>
+#include <cmath>
 
-#include <random.hpp>
-#include <ann.hpp>
+
+using namespace std;
 
 
-int main() {
-    ma::random::seed();
 
-    vector<int> sizes;
-    sizes.push_back(3);
-    sizes.push_back(10);
-    sizes.push_back(5);
-    ma::ann_leaner<double> nn(sizes);
+double mul_div(double* o, size_t size) {
+    double res = 1.;
 
-    nn.reset_deriv_for_next_minibatch();
+    size_t idx_o = 0;
+    double idx_n = 1;
 
-    double x[] = {1,2,3,  2,5,3};
-    double y[] = {0, 0, 0, 1, 0,  0, 0, 1, 0, 0};
+    while (idx_o < size && idx_n <= size) {
+        res *= (o[idx_o] / idx_n);
+        idx_o += 1;
+        idx_n += 1;
 
-/*
-    nn.forward(&x[0]);
-    double cost = nn.backward(&x[0], &y[0]);
-    nn.forward(&x[3]);
-    cost += nn.backward(&x[3], &y[5]);
-    nn.average_deriv(2.);
-*/
+        while (res > 1000000000. && idx_n <= size)
+            res /= idx_n++;
 
-    double cost = nn.fit_minibatch(x, y, 2, .2);
+        while (res < .0000000001 && idx_o < size)
+            res *= o[idx_o++];
+    }
 
-    nn.print(cout);
-    cout << "Cost: " << (cost / 2.) << endl;
+    while (idx_o < size)
+        res *= o[idx_o++];
+
+    while (idx_n <= size)
+        res /= idx_n++;
+
+    return res;
+}
+
+
+double sum_sub(double* s, size_t size) {
+    size_t idx_n = 1;
+
+    ssize_t res = 0;
+    for (size_t i = 0; i < size; ++i)
+        res += ((idx_n++) - (size_t)s[i]);
+    return res;
+}
+
+void shuffle(double* o, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        size_t idx = (rand() % size);
+        double tmp = o[i];
+        o[i] = o[idx];
+        o[idx] = tmp;
+    }
+}
+
+void fill(double* o, size_t size) {
+    for (size_t i = 0; i < size; ++i)
+        o[i] = (double)(i+1);
+}
+
+
+
+
+void print(ostream& os, double* o, size_t size) {
+    os << "double orig[" << size << "] = {";
+    for (size_t i = 0; i < size; ++i)
+        os << o[i] << ",";
+    os << "};" << endl;
+}
+
+
+
+int run(double* o, size_t N) {
+    fill(&o[0], N);
+//    print(cout, o, N);
+    shuffle(o, N);
+//    print(cout, o, N);
+
+
+    // miss + dup
+    size_t idx_missed = rand() % N;
+    size_t idx_duplicated = rand() % N;
+    while (idx_duplicated == idx_missed)
+        idx_duplicated = rand() % N;
+
+    double missed = o[idx_missed];
+    double duplicated = o[idx_duplicated];
+
+    o[idx_missed] = duplicated;
+
+    cout << "Missed: " << (size_t)missed << endl
+         << "Duplicated: " << (size_t)duplicated << endl;
+//    print(cout, o, N);
+
+
+
+    double k = mul_div(o, N);
+    cout << "MUL_DIV: " << k << endl;
+
+    double ss = sum_sub(o, N);
+
 
     //
-    int total_bd_size = 10 + 5;
-    int total_wd_size = 3*10 + 10*5;
+    double m = round(ss / (1. - k));
+    double d = round(k * m);
+    int err = fabs(missed - m) + fabs(duplicated - d);
 
-    ma::memory::ptr_vec<double> bd_accum(new double[total_bd_size]);
-    ma::memory::ptr_vec<double> wd_accum(new double[total_wd_size]);
-    ma::linalg::fill(bd_accum.get(), total_bd_size, (double)0);
-    ma::linalg::fill(wd_accum.get(), total_wd_size, (double)0);
+    cout << "Calculated result" << endl;
+    cout << "Missed: " << (size_t)m << endl
+         << "Duplicated: " << (size_t)d << endl
+         << "Error: " << err << endl;
 
-    {
-        ma::memory::ptr_vec<double> bd, wd;
 
-        nn.calc_deriv(&x[0], &y[0], bd, wd);
-        ma::linalg::sum_v2v(bd_accum.get(), bd.get(), total_bd_size);
-        ma::linalg::sum_v2v(wd_accum.get(), wd.get(), total_wd_size);
+    return err;
+}
 
-        nn.calc_deriv(&x[3], &y[5], bd, wd);
-        ma::linalg::sum_v2v(bd_accum.get(), bd.get(), total_bd_size);
-        ma::linalg::sum_v2v(wd_accum.get(), wd.get(), total_wd_size);
 
-        ma::linalg::div_v2s(bd_accum.get(), total_bd_size, 2.);
-        ma::linalg::div_v2s(wd_accum.get(), total_wd_size, 2.);
+int main(int argc, const char** argv) {
+
+    srand(time(NULL));
+
+    if (argc > 1) {
+        size_t N = atoi(argv[1]);
+        cout << "N = " << N << endl;
+
+        double* o = new double[N];
+        int err = run(o, N);
+        delete [] o;
 
     }
+    else {
+        size_t N = 500000000;
+        double* o = new double[N];
 
-
-    cout << "BD vs BB_DERIV" << endl;
-    for (int b = 0; b < total_bd_size; ++b)
-        cout << bd_accum[b] << "\t" << nn.get_bb_deriv()[b] << endl;
-
-    cout << "WD vs WW_DERIV" << endl;
-    for (int w = 0; w < total_wd_size; ++w)
-        cout << wd_accum[w] << "\t" << nn.get_ww_deriv()[w] << endl;
-
-    double b_error = 0;
-    double w_error = 0;
-
-    for (int b = 0; b < total_bd_size; ++b)
-        b_error += ::sqrt((bd_accum[b] - nn.get_bb_deriv()[b]) * (bd_accum[b] - nn.get_bb_deriv()[b]));
-
-    for (int w = 0; w < total_wd_size; ++w)
-        w_error += ::sqrt((wd_accum[w] - nn.get_ww_deriv()[w]) * (wd_accum[w] - nn.get_ww_deriv()[w]));
-
-    cout << "BD avr error: " << (b_error / total_bd_size) << endl;
-    cout << "WD avr error: " << (w_error / total_wd_size) << endl;
-
-    // test for decreasing cost
-    double alpha = 7.6;
-    for (int i = 0; i < 10000; ++i) {
-        cost = nn.fit_minibatch(x, y, 2, alpha);
-        if (0 == (i % 500)) {
-            cout << "Cost: " << cost << endl;
-            //alpha -= .01;
+        for (size_t n = N; n > 1; --n) {
+            cout << "N = " << n << endl;
+            for (int t = 0; t < 10; ++t) {
+                int err = run(o, n);
+                if (err) {
+                    // todo
+                }
+            }
         }
+        delete [] o;
     }
-    cout << "Cost: " << cost << endl;
-
-
 
     return 0;
 }
